562be84b54dc65c2755d767cb9aa61dfa9c8112c
hamadaldokhail <80049691+hamadaldokhail@users.noreply.github.com>
2025-10-22 15:04:20 +0300

    fixed spelling

--- src/LibrarySimulator.java
+++ src/LibrarySimulator.java
@@ -1,6 +1,6 @@
 // Khaled Abdulaziz ALquwaizany ID 446106134
 // Yasser Abdullah Albusairy	ID 446104619
-// Hamad Ahmed Aldokhail		ID 446103564
+// Hamed Ahmed Aldkhyyal		ID 446103564
 
 import java.util.Scanner;
 
@@ -227,7 +227,7 @@ public class LibrarySimulator {
 					System.out.print("Enter admin username: ");
 					username = input.next();
 
-					System.out.print("Enter passowrd: ");
+					System.out.print("Enter password: ");
 					password = input.next();
 
 					if (username.equalsIgnoreCase("admin") && password.equals("admin")) {
